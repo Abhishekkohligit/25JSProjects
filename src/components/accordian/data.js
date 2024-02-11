69cf2877cdde99a7d694d30b7b052ee29030ca83
Abhishek Kohli <ab02110211@gmail.com>
2024-02-11 12:57:20 +0000

    challenge 1 Accordian done

---
+++ src/components/accordian/data.js
@@ -0,0 +1,29 @@
+const data = [
+	{
+		id: 1,
+		question: "what is accordian",
+		answer:
+			'Accordions (from 19th-century German Akkordeon, from Akkord—"musical chord, concord of sounds")[1] are a family of box-shaped musical instruments of the bellows-driven free reed aerophone type (producing sound as air flows past a reed in a frame). ',
+	},
+
+	{
+		id: 2,
+		question: "how old is accordian",
+		answer:
+			"The accordion's basic form is believed to have been invented in Berlin, in 1822, by Christian Friedrich Ludwig Buschmann,[notes 2][6] although one instrument was discovered in 2006 that appears to have been built earlier.[notes 3][7][8]",
+	},
+	{
+		id: 3,
+		question: "How is accordian made",
+		answer:
+			"Accordions have many configurations and types. What may be easy to do with one type of accordion could be technically challenging or impossible with another, and proficiency with one layout may not translate to another",
+	},
+	{
+		id: 4,
+		question: " How is accordian used",
+		answer:
+			"The accordion has traditionally been used to perform folk or ethnic music, popular music, and transcriptions from the operatic and light-classical music repertoire.[30] It was also used by the Kikuyu tribe in Kenya and is the main instrument in the traditional Mwomboko dance.[31] Today the instrument is sometimes heard in contemporary pop styles, such as rock and pop-rock,[32] and occasionally even in serious classical music concerts, as well as advertisements.",
+	},
+];
+
+export default data;
---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,74 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { useState } from "react";
+import data from "./data";
+import "./styles.css";
+
+export default function Accordian() {
+	const [selected, setSelected] = useState(null);
+	const [enableMultipleSelection, setEnableMultipleSelection] = useState(false);
+
+	const [multipleSelection, setMultipleSelection] = useState([]);
+
+	function handleSingleSelection(currentId) {
+		setSelected(currentId === selected ? null : currentId);
+	}
+
+	function handleMultipleselection(currentId) {
+		let copyArray = [...multipleSelection];
+
+		const indexOfCurrentId = copyArray.indexOf(currentId);
+		if (indexOfCurrentId === -1) {
+			copyArray.push(currentId);
+		} else {
+			copyArray.splice(indexOfCurrentId, 1);
+		}
+
+		setMultipleSelection(copyArray);
+	}
+
+	return (
+		<div className="wrapper">
+			<button
+				className={`${enableMultipleSelection && "enabled"}`}
+				onClick={() => setEnableMultipleSelection(!enableMultipleSelection)}
+			>
+				{enableMultipleSelection
+					? "Multiple selection Enabled"
+					: "Enable Multiple selection"}
+			</button>
+			<div className="accordian">
+				{data && data.length > 0 ? (
+					data.map((dataItem) => (
+						<div key={dataItem.id} className="item">
+							<div
+								className="title"
+								onClick={
+									enableMultipleSelection
+										? () => handleMultipleselection(dataItem.id)
+										: () => handleSingleSelection(dataItem.id)
+								}
+							>
+								<h3>{dataItem.question}</h3>
+								<span>+</span>
+							</div>
+							{enableMultipleSelection
+								? multipleSelection.indexOf(dataItem.id) !== -1 && (
+										<div className="content">{dataItem.answer}</div>
+								  )
+								: selected === dataItem.id && (
+										<div className="content">{dataItem.answer}</div>
+								  )}
+
+							{/* {selected === dataItem.id ||
+							multipleSelection.indexOf(dataItem.id !== -1) ? (
+								<div className="content">{dataItem.answer}</div>
+							) : null} */}
+						</div>
+					))
+				) : (
+					<div> no data found</div>
+				)}
+			</div>
+		</div>
+	);
+}
